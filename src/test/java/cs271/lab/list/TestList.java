107794f0d8840dd5f720a38b8247f2ed5d5f7d2e
Skye <skyel1005@gmail.com>
2026-09-18 17:45:20 -0500

    Remove appropriate items from list using retainAll

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -173,6 +173,7 @@ public class TestList {
     // TODO in a single statement using retainAll and List.of,
     // remove items from the list to make the following assertions pass
     // (without touching the assertions themselves)
+    list.retainAll(List.of(77));
     assertEquals(3, list.size());
     assertEquals(List.of(77, 77, 77), list);
   }
