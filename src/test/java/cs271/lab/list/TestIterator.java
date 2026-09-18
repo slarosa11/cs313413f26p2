693af40a81ce9ccaec7e5feb1483757937253800
Skye <skyel1005@gmail.com>
2026-09-18 17:55:37 -0500

    Add code to compute mean of the values in testAverageValues

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -98,6 +98,11 @@ public class TestIterator {
     // TODO use an iterator and a while loop to compute the average (mean) of the values
     // (defined as the sum of the items divided by the number of items)
     // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+    final var i=list.iterator();
+    while (i.hasNext()) {
+      sum += i.next();
+      n++;
+    }
     assertEquals(61.3, sum / n, 0.1);
     assertEquals(7, n);
   }
