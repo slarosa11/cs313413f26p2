a9078e2e12fa9a093b6e8a18b8aec619ef7ed9de
Skye <skyel1005@gmail.com>
2026-09-18 18:03:14 -0500

    Edit wrong index in testSet Edit TODO comments for organization

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -21,7 +21,8 @@ public class TestIterator {
   @BeforeEach
   public void setUp() throws Exception {
     list = new ArrayList<Integer>();
-    // TODO Question: Also try with a LinkedList - does it make any difference?
+    // Question: Also try with a LinkedList - does it make any difference?
+
   }
 
   @AfterEach
@@ -47,7 +48,6 @@ public class TestIterator {
     final var i = list.iterator();
     assertTrue(i.hasNext());
     assertEquals(33, i.next().intValue());
-    // TODO fix the expected values in the assertions below
     assertTrue(i.hasNext());
     assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
@@ -78,7 +78,7 @@ public class TestIterator {
         i.remove(); // list.remove(Integer.valueOf(77)) would cause an error since it modifies a list while an iterator is moving over it
       }
     }
-    // TODO using assertEquals and List.of, express which values are left in the list
+    // using assertEquals and List.of, express which values are left in the list
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
     assertEquals(List.of(33, 44, 55, 66), list);
@@ -95,7 +95,7 @@ public class TestIterator {
     list.add(66);
     double sum = 0;
     int n = 0;
-    // TODO use an iterator and a while loop to compute the average (mean) of the values
+    // use an iterator and a while loop to compute the average (mean) of the values
     // (defined as the sum of the items divided by the number of items)
     // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
     final var i=list.iterator();
--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -20,7 +20,7 @@ public class TestList {
   @BeforeEach
   public void setUp() throws Exception {
     list = new ArrayList<Integer>();
-    // TODO Question: Also try with a LinkedList - does it make any difference?
+    // Question: Also try with a LinkedList - does it make any difference?
   }
 
   @AfterEach
@@ -41,7 +41,7 @@ public class TestList {
 
   @Test
   public void testSizeNonEmpty() {
-    // TODO fix the expected values in the assertions below
+    // fix the expected values in the assertions below
     list.add(77);
     assertEquals(false, list.isEmpty());
     assertEquals(1, list.size());
@@ -50,7 +50,7 @@ public class TestList {
 
   @Test
   public void testContains() {
-    // TODO write assertions using
+    // write assertions using
     // list.contains(77)
     // that hold before and after adding 77 to the list
     assertFalse(list.contains(77));
@@ -63,7 +63,7 @@ public class TestList {
     list.add(77);
     list.add(77);
     list.add(77);
-    // TODO fix the expected values in the assertions below
+    // fix the expected values in the assertions below
     assertEquals(3, list.size());
     assertEquals(0, list.indexOf(77));
     assertEquals(77, list.get(1).intValue());
@@ -79,7 +79,7 @@ public class TestList {
     list.add(55);
     list.add(77);
     list.add(66);
-    // TODO fix the expected values in the assertions below
+    // fix the expected values in the assertions below
     assertEquals(7, list.size());
     assertEquals(1, list.indexOf(77));
     assertEquals(5, list.lastIndexOf(77));
@@ -98,7 +98,7 @@ public class TestList {
     list.add(77);
     list.add(6);
     list.remove(5); // This removes what is at position 5, which is 77
-    // TODO fix the expected values in the assertions below
+    // fix the expected values in the assertions below
     assertEquals(6, list.size());
     assertEquals(1, list.indexOf(77));
     assertEquals(3, list.lastIndexOf(77));
@@ -121,7 +121,7 @@ public class TestList {
     list.add(55);
     list.add(77);
     list.add(66);
-    // TODO using containsAll and List.of (see above),
+    // using containsAll and List.of (see above),
     // 1) assert that list contains all five different numbers added
     // 2) assert that list does not contain all of 11, 22, and 33
     assertTrue(list.containsAll(List.of(33, 77, 44, 55, 66)));
@@ -130,7 +130,7 @@ public class TestList {
 
   @Test
   public void testAddAll() {
-    // TODO in a single statement using addAll and List.of,
+    // in a single statement using addAll and List.of,
     // add items to the list to make the following assertions pass
     // (without touching the assertions themselves)
     list.addAll(List.of(33, 77, 44, 77, 55, 77, 66));
@@ -153,7 +153,7 @@ public class TestList {
     list.add(55);
     list.add(77);
     list.add(66);
-    // TODO in a single statement using removeAll and List.of,
+    // in a single statement using removeAll and List.of,
     // remove items from the list to make the following assertions pass
     // (without touching the assertions themselves)
     list.removeAll(List.of(33, 44, 55, 66));
@@ -170,7 +170,7 @@ public class TestList {
     list.add(55);
     list.add(77);
     list.add(66);
-    // TODO in a single statement using retainAll and List.of,
+    // in a single statement using retainAll and List.of,
     // remove items from the list to make the following assertions pass
     // (without touching the assertions themselves)
     list.retainAll(List.of(77));
@@ -187,11 +187,11 @@ public class TestList {
     list.add(55);
     list.add(77);
     list.add(66);
-    // TODO use the set method to change specific elements in the list
+    // use the set method to change specific elements in the list
     // such that the following assertions pass
     // (without touching the assertions themselves)
     list.set(1,99);
-    list.set(2,99);
+    list.set(3,99);
     list.set(5,99);
     assertEquals(7, list.size());
     assertEquals(33, list.get(0).intValue());
@@ -212,7 +212,7 @@ public class TestList {
     list.add(55);
     list.add(77);
     list.add(66);
-    // TODO fix the arguments in the subList method so that the assertion
+    // fix the arguments in the subList method so that the assertion
     // passes
     assertEquals(List.of(44, 77, 55), list.subList(2, 5));
   }
